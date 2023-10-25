46a31494fe3934a0c316715c4ab5a945e8b1cf58
ismailostmar <ismailostmar@gmail.com>
2023-10-25 16:45:30 +0200

    testBucket

--- main.tf
+++ main.tf
@@ -5,6 +5,11 @@
 provider "aws" {
   region     = "us-east-1"
 }
+
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "IsmailBucket"
+}
+
 /* 
 ##################################################################################
 # DATA
